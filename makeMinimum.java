6e089fd1fc6301b36c93bcc937daf881b57205ab
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-01-02 21:48:30 +0900

    # 최소값 만들기[Lv2] - 문제에 해당하는 알고리즘이 무엇인지 주석화

---
+++ makeMinimum.java
@@ -0,0 +1,26 @@
+/*
+이 문제를 카테고리화하자면, 그리디 알고리즘임
+최선의 방법을 찾는건데, 보통 최대 혹은 최소를 다루는 경우임
+항상 로직을 의심해야함
+:: 이 로직이 확실히 최대 혹은 최소를 구하는 로직인가?
+
+최대, 최소를 사용하다보면 정렬을 자주 사용해야하는데
+사용하는 정렬의 알고리즘을 상황에 맞게 선택하여
+최소한의 시간과 메모리를 사용해야하는 게 관건임
+ */
+
+// Arrays.sort()(:: int[]), Collections.sort()(:: List<Integer>
+// 중 무엇이 더 효율적일까
+//
+public class makeMinimum {
+    public static void main(String[] args) {
+        int min = solution(new int[]{1, 4, 2}, new int[]{5, 4, 4});
+    }
+
+    public static int solution(int[] A, int[] B) {
+        int answer = 0;
+
+
+        return answer;
+    }
+}
