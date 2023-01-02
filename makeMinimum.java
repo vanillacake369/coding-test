762e9bfd79bd1f8049548b47a00ab72843b35422
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-01-03 03:20:27 +0900

    - Arrays.sort()를 통해 quick sort

--- makeMinimum.java
+++ makeMinimum.java
@@ -1,17 +1,7 @@
-/*
-이 문제를 카테고리화하자면, 그리디 알고리즘임
-최선의 방법을 찾는건데, 보통 최대 혹은 최소를 다루는 경우임
-항상 로직을 의심해야함
-:: 이 로직이 확실히 최대 혹은 최소를 구하는 로직인가?
-
-최대, 최소를 사용하다보면 정렬을 자주 사용해야하는데
-사용하는 정렬의 알고리즘을 상황에 맞게 선택하여
-최소한의 시간과 메모리를 사용해야하는 게 관건임
- */
+import java.util.Arrays;
 
 // Arrays.sort()(:: int[]), Collections.sort()(:: List<Integer>
-// 중 무엇이 더 효율적일까
-//
+// Collections.sort()는 내부적으로 Arrays.sort()를 사용함!
 public class makeMinimum {
     public static void main(String[] args) {
         int min = solution(new int[]{1, 4, 2}, new int[]{5, 4, 4});
@@ -19,8 +9,19 @@ public class makeMinimum {
 
     public static int solution(int[] A, int[] B) {
         int answer = 0;
-
-
+        Arrays.sort(A);
+        int[] AscA = A;
+        /* int[] DescB = Arrays.stream(B)
+                .boxed()
+                .sorted(Collections.reverseOrder())
+                .mapToInt(Integer::intValue)
+                .toArray(); */
+        Arrays.sort(B);
+        int[] AscB = B;
+        // A의 최대 x B의 최소
+        for (int i = 0; i < A.length; i++) {
+            answer += (AscA[i] * AscB[B.length - 1 - i]);
+        }
         return answer;
     }
 }
