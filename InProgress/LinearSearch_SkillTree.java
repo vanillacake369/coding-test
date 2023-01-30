aabb55c8ab7092f21da94e633e80f37dcd20fa61
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-01-30 20:06:47 +0900

    linear search "skill tree문제" : 선형탐색 시 정규식을 사용할 수도 있다

--- InProgress/LinearSearch_SkillTree.java
+++ InProgress/LinearSearch_SkillTree.java
@@ -1,6 +1,8 @@
 package InProgress;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Hashtable;
 import java.util.List;
 
 public class LinearSearch_SkillTree {
@@ -11,42 +13,61 @@ public class LinearSearch_SkillTree {
     }
 
     public static int solution(String skill, String[] skill_trees) {
-        // 큐 생성
-        List<String> skillQueue = new ArrayList<>();
-        // 큐에 대한 포인터
+        // 정규화 :: Stream 사용
+        return (int) Arrays.stream(skill_trees)
+                .map(s -> s.replaceAll("[^" + skill + "]", ""))
+                .filter(s -> skill.startsWith(s))
+                .count();
+        // 정규화 :: for문 사용
+        /*int answer = 0;
+        for (String s : skill_trees) {
+            String s2 = s.replaceAll("[^" + skill + "]", "");
+            if (skill.startsWith(s2)) answer++;
+        }
+        return answer;*/
+
+
+        /*// 해시테이블 생성
+        Hashtable<String, Integer> stringIntegerHashtable = new Hashtable<>();
+        // 포인터
         int index = 0;
         // 맞는 스킬트리 count
         int possibleSkillTree = 0;
 
-        // 스킬들(skill) 속 스킬 하나하나 => 큐
+        // 스킬들(skill) 속 스킬 하나하나 :: 스킬(key)=>순서(value)
         for (int i = 0; i < skill.length(); i++) {
-            skillQueue.add(Character.toString(skill.charAt(i)));
+            String sk = Character.toString(skill.charAt(i));
+            stringIntegerHashtable.put(sk, i);
         }
+
         // 스킬 트리 속 스킬과 스킬 비교 :: 스킬 일치 시 포인터 이동
         for (int i = 0; i < skill_trees.length; i++) {
+            if (index >= stringIntegerHashtable.size()) {
+                break;
+            }
             String skills = skill_trees[i];
             String s = null;
+            boolean isPossible = true;
             for (int j = 0; j < skills.length(); j++) {
-                String seq = skillQueue.get(index);
-                System.out.println("seq : " + seq);
                 s = Character.toString(skills.charAt(j));
-                System.out.println("s : " + s);
-                if ((index < skillQueue.size()) && (seq.equals(s))) {
-                    System.out.println("match : " + s + " and " + seq);
-                    ++index;
-                    System.out.println("index : " + index);
-                }
-                // 포인터가 끝까지 이동 => 스킬트리 개수 count
-                else if (index >= skillQueue.size()) {
-                    ++possibleSkillTree;
+                int seq = stringIntegerHashtable.getOrDefault(s, -1);
+                if (seq > index) {
+                    isPossible = false;
+                    break;
+                } else if (seq == index) {
+                    index++;
+                } else {
+                    continue;
                 }
             }
-
+            if (isPossible == true) {
+                possibleSkillTree++;
+            } else {
+                continue;
+            }
             // 포인터 초기화
             index = 0;
-            System.out.println("count possible tree : " + possibleSkillTree);
         }
-
-        return possibleSkillTree;
+        return possibleSkillTree;*/
     }
 }
