c38ac59726f11b18c136ac164f6a74a619f308cd
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-05-02 13:40:49 +0900

    재귀호출 > 팩토리얼

---
+++ Codetree/ReturnRecursive/Factorial.java
@@ -0,0 +1,25 @@
+package Codetree.ReturnRecursive;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        /* 입력 */
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        /* 팩토리얼 연산 */
+        int factResult = getFactorial(n);
+
+        /* 결과 */
+        System.out.println(factResult);
+    }
+
+    private static int getFactorial(int n) {
+        if (n == 0)
+            return 1;
+        if (n == 1)
+            return 1;
+        return n * getFactorial(n - 1);
+    }
+}
