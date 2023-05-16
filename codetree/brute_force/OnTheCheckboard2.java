72ad15a9dca6696d07bc59d8e7031d7a81bfa7c1
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-05-16 14:04:18 +0900

    완전탐색 > 최고의 13위치

---
+++ codetree/brute_force/OnTheCheckboard2.java
@@ -0,0 +1,17 @@
+package codetree.brute_force;
+
+public class OnTheCheckboard2 {
+
+    /**
+     * 의사코드
+     *
+     * 1. (i,j)의 색을 저장한다.
+     * 2. n-i 열과 n-j열에 반대색이 있는지 탐색한다.
+     * 3. 없다면 다음 줄을 탐색, 있다면 점프한다.
+     *      3-1. (0,0) : '색' => (a,b) : '반대색'
+     * 4.
+     */
+    public static void main(String[] args) {
+
+    }
+}
