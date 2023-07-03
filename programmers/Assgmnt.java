a3e6a270f3347520b53338ed9852c9bb37276243
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-07-03 18:07:26 +0900

    1일 1코테 > 프로그래머스 Lv1

---
+++ programmers/Assgmnt.java
@@ -0,0 +1,116 @@
+package programmers;
+
+import java.util.*;
+
+public class Assgmnt {
+    // https://school.programmers.co.kr/learn/courses/30/lessons/176962
+    public static void main(String[] args) {
+        String[] answer = solution(new String[][]{
+                {"korean", "11:40", "30"},
+                {"english", "12:10", "20"},
+                {"math", "12:30", "40"}});
+        for (String a : answer) {
+            System.out.println(a);
+        }
+    }
+
+    static class Class implements Comparable {
+        String lec;
+        int start;
+        int time;
+
+        public Class(String lec, String start, String time) {
+            this.lec = lec;
+            this.start = getStart(start);
+            this.time = Integer.parseInt(time);
+        }
+
+        public int getStart(String s) {
+            int h = Integer.parseInt(
+                    s.split(":")[0]
+            );
+            int m = Integer.parseInt(
+                    s.split(":")[1]
+            );
+            return h * 60 + m;
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            Class c = (Class) o;
+            return this.start - c.start;
+        }
+    }
+
+    public static String[] solution(String[][] plans) {
+        List<String> answer = new ArrayList<String>();
+
+//        Class[] plansC = new Class[plans.length];
+        List<Class> plansC = new ArrayList<Class>();
+        for (int i = 0; i < plans.length; i++) {
+            plansC.add(new Class(plans[i][0], plans[i][1], plans[i][2]));
+        }
+        plansC.stream().sorted();
+
+        int next = 0;
+        int now = plansC.get(next).start;
+        Stack<Class> stop = new Stack<>();
+
+        // 과제가 다 완료될 때까지
+        while (answer.size() != plans.length) {
+            // 과제 못 끝내고 넘어가야할 때
+            if (now + plansC.get(next).time
+                    > plansC.get(next + 1 % plansC.size()).start) {
+                Class notDone = plansC.get(next);
+                int notDoneTime = notDone.time;
+                // 과제 진행시간 삭감
+                notDone.time -= (plansC.get(next + 1 % plansC.size()).start - now);
+                // 현재 시간 업데이트
+                now += notDoneTime;
+                // 멈춘 과제 스택에 푸시
+                stop.push(notDone);
+                // 과제 플랜 리스트에서 삭제
+                plansC.remove(next);
+                // 다음 과제로 넘어감
+                next = next + 1 % plans.length;
+                continue;
+            }
+            // 과제 딱 맞춰서 끝냈을 때
+            if (now + plansC.get(next).time
+                    == plansC.get(next + 1 % plansC.size()).start) {
+                // 정답 리스트에 추가
+                answer.add(plansC.get(next).lec);
+                // 과제 플랜 리스트에서 삭제
+                plansC.remove(next);
+                // 다음 과제로 넘어감
+                next = next + 1 % plans.length;
+                continue;
+            }
+            // 과제 끝내고 시간 남음
+            if (now + plansC.get(next).time
+                    < plansC.get(next + 1 % plansC.size()).start) {
+                Class stopped = stop.pop();
+                // 남은 시간 동안 멈춘 과제 끝냄
+                if (plansC.get(next + 1).start - now
+                        > stopped.time) {
+                    // 정답 리스트에 추가
+                    answer.add(plansC.get(next).lec);
+                } else { // 남은 시간 동안 멈춘 과제 못 끝냄
+                    int stoppedTime = stopped.time;
+                    // 과제 진행시간 삭감
+                    stopped.time -= (plansC.get(next + 1 % plansC.size()).start - now);
+                    // 현재 시간 업데이트
+                    now += stoppedTime;
+                    // 멈춘 과제 스택에 푸시
+                    stop.push(stopped);
+                }
+                // 다음 과제로 넘어감
+                next = next + 1 % plans.length;
+                continue;
+            }
+        }
+
+        return answer.toArray(String[]::new);
+    }
+
+}
---
+++ programmers/Keyboard.java
@@ -0,0 +1,49 @@
+package programmers;
+//https://school.programmers.co.kr/learn/courses/30/lessons/160586
+
+import java.sql.SQLOutput;
+import java.util.Hashtable;
+
+public class Keyboard {
+    public static void main(String[] args) {
+//        int[] answer = solution(new String[]{"ABACD", "BCEFD"}, new String[]{"ABCD", "AABB"});
+//        int[] answer = solution(new String[]{"AA"}, new String[]{"B"});
+//        int[] answer = solution(new String[]{"AGZ", "BSSS"}, new String[]{"ASA", "BGZ"});
+//        int[] answer = solution(new String[]{"ABCE"}, new String[]{"ABDE"});
+//        for (int a : answer) {
+//            System.out.println(a);
+//        }
+    }
+
+    public int[] solution(String[] keymap, String[] targets) {
+        Hashtable<Character, Integer> table = new Hashtable<>();
+        for (String k : keymap) {
+            for (int i = 0; i < k.length(); i++) {
+                char ch = k.charAt(i);
+                if (table.get(ch) != null) {
+                    table.put(
+                            ch,
+                            Math.min(table.get(ch), i + 1)
+                    );
+                } else {
+                    table.put(ch, i + 1);
+                }
+            }
+        }
+        int[] arr = new int[targets.length];
+        for (int i = 0; i < targets.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < targets[i].length(); j++) {
+                char ch = targets[i].charAt(j);
+                if (table.get(ch) == null) {
+                    sum = -1;
+                    break;
+                } else {
+                    sum += table.get(ch);
+                }
+            }
+            arr[i] = sum;
+        }
+        return arr;
+    }
+}
