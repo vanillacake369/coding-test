a7537e16e656f12dc1cb58359041c11919150693
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-11-16 15:11:20 +0900

    프로그래머스 > 가장 가까운 같은 글자

---
+++ programmers/가장 가까운 같은 글자.py
@@ -0,0 +1,16 @@
+def solution(s):
+    answer = []
+    mem = {}
+    for i in range(0,len(s)):
+        if((s[i] in mem) == False):
+            answer.append(-1)
+            mem[s[i]] = i
+        else:
+            diff = i - mem[s[i]]
+            answer.append(diff)
+            mem[s[i]] = i
+
+    return answer
+
+s = "banana"	
+print(solution(s))
