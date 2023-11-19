6b95ea754ff666866686187eb49247e16650c397
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-11-19 15:25:30 +0900

    코딩테스트 연습 연습문제 콜라 문제

--- programmers/스파르타_스터디/콜라문제.py
+++ programmers/스파르타_스터디/콜라문제.py
@@ -1,16 +1,25 @@
-# 점화식 
-# k = n ~ a까지
-# sum of (k - k//a*a + k//a)
-
 def solution(a, b, n):
     answer = 0
+    k = n
+    while(k>=a):
+        # 가져갈 병수
+        empt = k // a * a
+        # 받은 병수
+        given = empt // a * b
+        # 가지고 있는 콜라 수
+        k = k - empt + given
+        # 받은 콜라와 가지고 있는 콜라 업데이트
+        answer += given
     return answer
 
 
-
-a=2	
-b= 1	
+# a=2	
+# b= 1	
+# n=20	
+# result = 19
+a=3
+b=1	
 n=20	
-result = 19
-print(solution(2,1,20) == result)
-print(solution(2,1,20))
+result = 9
+print(solution(a,b,n) == result)
+print(solution(a,b,n))
