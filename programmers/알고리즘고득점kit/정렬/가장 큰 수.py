e61a277ec346caf9ac0c86f769c40a9c6e539979
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-11-23 01:50:46 +0900

    프로그래머스 > 정렬> 가장큰수

---
+++ programmers/알고리즘고득점kit/정렬/가장 큰 수.py
@@ -0,0 +1,11 @@
+def solution(numbers):
+    answer = ''
+    numStr = str(numbers)
+    print(numStr[0])
+    print(numStr[1])
+    print(numStr[2])
+    # print(numStr[3])
+    return answer
+
+numbers = 333
+solution(numbers)
