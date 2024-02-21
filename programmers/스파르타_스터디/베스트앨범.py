1237efbe6cb52da171af87a995f7369b1da4acfc
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-02-21 19:38:39 +0900

    프그 > 해시 > 베스트앨범 > 주석 수정

--- programmers/스파르타_스터디/베스트앨범.py
+++ programmers/스파르타_스터디/베스트앨범.py
@@ -23,11 +23,11 @@ def solution(genres, plays):
             playPerGenres[g] = []
         playPerGenres[g].append(play)
 
-    #3 "재생횟수" 기준 "내림차순"으로 고유번호 정렬
+    #3 "재생횟수" 기준 "내림차순"으로 playPerGenres 정렬
     sorted_playPerGenres = {genre: sorted(genre_data, key=lambda x: list(x.values())[0], reverse=True) for genre, genre_data in playPerGenres.items()}
     print(sorted_playPerGenres)
     
-    #4 전체횟수 내림차순으로 순회, 모든 장르 중 고유번호 최소갯수만큼 고유번호 추출, result 리스트에 append
+    #4 "총 재생횟수" 기준 "내림차순" 으로 total_plays_per_genre 정렬
     sorted_genres = sorted(total_plays_per_genre.keys(), key=lambda x: -total_plays_per_genre[x])
 
     #5 장르 별 최대 2 노래 추가
