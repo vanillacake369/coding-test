76c05f3be5ef922cb473f06c445c5968f84dbea2
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-02-21 19:04:22 +0900

    프그 > 해시 > 베스트앨범 > 통과

--- programmers/스파르타_스터디/베스트앨범.py
+++ programmers/스파르타_스터디/베스트앨범.py
@@ -1,24 +1,21 @@
-# 장르 별로 가장 많이 재생된 노래를 최대 두 개까지 모아 ,,,
-# => "두 개까지 모은다" 의 기준은 어디있음??
-
 """
-1. [장르 : 전체 횟수]
-2. 장르 => [고유번호 : 재생횟수]
+1. {장르 : 전체횟수} 구한 뒤, "전체횟수" 기준 "내림차순" 정렬
+2. {장르 : [ {고유번호:재생횟수},{고유번호:재생횟수} ,,, ]}
 3. "재생횟수" 기준 "내림차순"으로 고유번호 정렬
-4. 각 장르의 고유번호 갯수 중 최소값 구하기
-5. 최소값에 따른 고유번호 poll, add
- """
+4. 전체횟수 내림차순으로 순회, 
+   모든 장르 중 고유번호 최소갯수만큼 고유번호 추출,
+   result 리스트에 append
+"""
 
 def solution(genres, plays):
     answer = []
 
-    #1
-    allPlays = dict()
+    #1 {장르 : 전체횟수} 
+    total_plays_per_genre = dict()
     for g,p in zip(genres,plays):
-        allPlays[g]=allPlays.get(g,0) + p
-    sortedAllPlays = dict(sorted(allPlays.items(),key=lambda x:x[1],reverse=True))
+        total_plays_per_genre[g]=total_plays_per_genre.get(g,0) + p
 
-    #2 
+    #2  {장르 : [ {고유번호:재생횟수},{고유번호:재생횟수} ,,, ]}
     playPerGenres = dict()
     for idx,g in enumerate(genres):
         play = {idx:plays[idx]}
@@ -26,17 +23,61 @@ def solution(genres, plays):
             playPerGenres[g] = []
         playPerGenres[g].append(play)
 
-    #3
+    #3 "재생횟수" 기준 "내림차순"으로 고유번호 정렬
     sorted_playPerGenres = {genre: sorted(genre_data, key=lambda x: list(x.values())[0], reverse=True) for genre, genre_data in playPerGenres.items()}
+    print(sorted_playPerGenres)
     
-    #4
-    for sap in sortedAllPlays :
-        for i in range(0,len(sorted_playPerGenres)):
-            answer.append(list(sorted_playPerGenres[sap][i].keys())[0])
+    #4 전체횟수 내림차순으로 순회, 모든 장르 중 고유번호 최소갯수만큼 고유번호 추출, result 리스트에 append
+    sorted_genres = sorted(total_plays_per_genre.keys(), key=lambda x: -total_plays_per_genre[x])
 
+    #5 장르 별 최대 2 노래 추가
+    for genre in sorted_genres:
+        count = 0
+        print( sorted_playPerGenres[genre])
+        for pg in sorted_playPerGenres[genre]:
+            for idx,play in pg.items():
+                answer.append(idx)
+            count+=1
+            if count >= 2:
+                break
+                
     return answer
+
 
 gen = ["classic", "pop", "classic", "classic", "pop"]	
 pl = [500, 600, 150, 800, 2500]	
 result = solution(gen,pl)
+result1 = solution1(gen,pl)
 print(result)
+print(result1)
+
+
+# def solution(genres, plays):
+#     answer = []
+
+#     #1 {장르 : 전체횟수} 구한 뒤, "전체횟수" 기준 "내림차순" 정렬
+#     total_plays_per_genre = dict()
+#     for g,p in zip(genres,plays):
+#         total_plays_per_genre[g]=total_plays_per_genre.get(g,0) + p
+#     sortedAllPlays = dict(sorted(total_plays_per_genre.items(),key=lambda x:x[1],reverse=True))
+
+#     #2  {장르 : [ {고유번호:재생횟수},{고유번호:재생횟수} ,,, ]}
+#     playPerGenres = dict()
+#     for idx,g in enumerate(genres):
+#         play = {idx:plays[idx]}
+#         if not (g in playPerGenres):
+#             playPerGenres[g] = []
+#         playPerGenres[g].append(play)
+
+#     #3 "재생횟수" 기준 "내림차순"으로 고유번호 정렬
+#     sorted_playPerGenres = {genre: sorted(genre_data, key=lambda x: list(x.values())[0], reverse=True) for genre, genre_data in playPerGenres.items()}
+    
+#     #4 전체횟수 내림차순으로 순회, 모든 장르 중 고유번호 최소갯수만큼 고유번호 추출, result 리스트에 append
+#     minPlayPerGenres = 100000000000
+#     for s in sorted_playPerGenres:
+#         minPlayPerGenres = min(minPlayPerGenres,len(sorted_playPerGenres[s]))
+#     for sap in sortedAllPlays:
+#         for i in range(0,minPlayPerGenres):
+#             answer.append(list(sorted_playPerGenres[sap][i].keys())[0])
+
+#     return answer
