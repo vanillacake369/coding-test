a81434caec7dac74913bd0055de7af285b95b57e
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-02-25 02:10:43 +0900

    프그 > 스택큐 > 기능개발

---
+++ programmers/스파르타_스터디/기능개발.py
@@ -0,0 +1,23 @@
+import math
+
+def solution(progresses, speeds):
+    answer = []
+
+    # 1
+    leftProgresses = [math.ceil((100 - p) / speeds[idx]) for idx, p in enumerate(progresses)]
+
+    leftPStack = [leftProgresses[0]]
+    daysToDeploy = 0
+    for idx,pr in enumerate(leftProgresses):
+        # 2
+        if(pr <= leftPStack[-1]):
+            daysToDeploy += 1
+        # 3
+        else:
+            answer.append(daysToDeploy)
+            leftPStack.append(pr)
+            daysToDeploy = 1
+    answer.append(daysToDeploy)
+    
+    # 4
+    return answer
