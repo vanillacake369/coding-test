f9452349b4ab35d56e196f20b1b5c204055e33e9
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-02-28 17:33:08 +0900

    프그 > 스택큐 > 프로세스 > deque 를 사용한 두 가지 풀이

--- programmers/스파르타_스터디/프로세스.py
+++ programmers/스파르타_스터디/프로세스.py
@@ -1,17 +1,46 @@
+# ====== deque 와 any 를 사용한 풀이 ======
 def solution(priorities, location):
+    queue = [(i, p) for i, p in enumerate(priorities)]
     answer = 0
-    priorities_location = {idx: p for idx, p in enumerate(priorities)}
-    sorted_priorities_location = dict(
-        sorted(
-            priorities_location.items(),
-            key=lambda x: (x[1], list(priorities_location.keys()).index(x[0])),
-            reverse=True,
-        )
-    )
-    print(sorted_priorities_location)
-    print(list(sorted_priorities_location.keys()))
-    answer = list(sorted_priorities_location.keys()).index(location) + 1
-    return answer
+    while True:
+        cur = queue.pop(0)
+        if any(cur[1] < q[1] for q in queue):
+            queue.append(cur)
+        else:
+            answer += 1
+            if cur[0] == location:
+                return answer
+
+
+# ====== deque 와 max 를 사용한 풀이 ======
+
+# from collections import deque
+
+# def solution(priorities, location):
+#     answer = 1
+#     d = deque(priorities)  # 새로 배우는 문법 : 1) deque() 2) deque(list)
+#     idx = location
+#     while len(d) > 1:
+#         tmp = d.popleft()
+#         # 1. 현재 프로세스가 max 가 아닌 경우, deque에 다시 append
+#         if tmp < max(d):  # 새로 배우는 문법 : max(deque)
+#             d.append(tmp)
+#             # 1-1. 만약 idx 가 0 인 경우,
+#             if idx == 0:
+#                 idx = len(d) - 1
+#             # 1-2. 0이 아닌 경우, idx 를 감소
+#             else:
+#                 idx -= 1
+#         # 2. 현재 프로세스가 max 인 경우
+#         else:
+#             # 2-1 현재 프로세스만 남은 경우라면 answer 반환
+#             if idx == 0:
+#                 return answer
+#             # 2-2 deque에 아직 프로세스가 남아있다면 answer,idx 계산 이후 다음 루프 실행
+#             else:
+#                 answer += 1
+#                 idx -= 1
+#     return answer
 
 
 # ---
