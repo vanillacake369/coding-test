3ee1f629d37e0f5128ba4e3b0765fdb4a934308f
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-03-05 23:14:38 +0900

    프그 > 다리를 지나는 트럭 > deque 사용한 풀이[정답]

--- programmers/스파르타_스터디/다리를 지나는 트럭.py
+++ programmers/스파르타_스터디/다리를 지나는 트럭.py
@@ -1,5 +1,6 @@
 from collections import deque
 
+
 def solution(bridge_length, weight, truck_weights):
     answer = 0
 
@@ -10,8 +11,8 @@ def solution(bridge_length, weight, truck_weights):
     while len(trucks) != 0:
         answer += 1
         currentWeight -= bridge.popleft()
-        if (currentWeight + trucks[0] <= weight):
-            currentWeight += truck_weights[0]
+        if currentWeight + trucks[0] <= weight:
+            currentWeight += trucks[0]
             bridge.append(trucks.popleft())
         else:
             bridge.append(0)
