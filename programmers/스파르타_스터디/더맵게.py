5f333df3e4fcf05b0af96a528988d8d5348da0e6
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-03-08 11:14:39 +0900

    프그 > 힙 > 더 맵게 > min 이 아닌 0 인덱스 조회를 통해 개선 > 통과

--- programmers/스파르타_스터디/더맵게.py
+++ programmers/스파르타_스터디/더맵게.py
@@ -4,15 +4,14 @@ import heapq
 def solution(scoville, K):
     mixCount = 0
     heapq.heapify(scoville)
-    while K > min(scoville) :
+    while K > scoville[0] :
         min1 = heapq.heappop(scoville)
         min2 = heapq.heappop(scoville)
         mixed = min1 + (min2 * 2)
-        if len(scoville) == 0:
-            if mixed < K:
-                return -1
         heapq.heappush(scoville, mixed)
         mixCount += 1
+        if len(scoville) == 1 and scoville[0] < K:
+            return -1
     return mixCount
 
 
