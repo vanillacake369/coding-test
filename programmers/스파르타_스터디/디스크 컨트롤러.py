fe29b1be9afa1e2789b9af0dc139b6d0a71df696
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-03-11 19:58:15 +0900

    프그 > 힙 > 디스크 컨트롤러 > 작업대기와 작업시간에 따른 우선순위큐를 사용 풀이 > 성공

--- programmers/스파르타_스터디/디스크 컨트롤러.py
+++ programmers/스파르타_스터디/디스크 컨트롤러.py
@@ -1,22 +1,31 @@
+import heapq
+
+
 def solution(jobs):
     answer = 0
-    sortedJobs = sorted(jobs, key=lambda x: x[1])
-    jobsLength = len(jobs)
+    now = 0  # 현재시간
+    count = 0  # 처리개수
+    start = -1  # 마지막 완료시간
+    heap = []
 
-    now = 0
-    while sortedJobs:
-        for j in jobs:
-            if j[0] <= now:
-                now += j[1]
-                answer += now - j[0]
-                sortedJobs.pop()
-                break
-            else:
-                now += 1
+    while count < len(jobs):
+        for job in jobs:
+            if start < job[0] <= now:
+                heapq.heappush(heap, [job[1], job[0]])
 
-    return answer // jobsLength
+        if heap:
+            current = heapq.heappop(heap)
+            start = now
+            now += current[0]
+            answer += now - current[1]  # 요청으로부터 처리시간
+            count += 1
+        else:
+            now += 1
+
+    return answer // len(jobs)
 
 
-jobs = [[0, 3], [1, 9], [2, 6]]
+jobs = [[0, 3], [1, 9], [2, 6]]  # 기대결과 : 9
+jobs = [[0, 3], [2, 1]]  # 기대결과 : 2
 result = solution(jobs)
 print(result)
