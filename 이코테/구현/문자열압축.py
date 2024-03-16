d252df8150a7a7b9323fda6e4126a59ed1ddc4c2
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-03-16 12:47:42 +0900

    구현 > 프그 > 문자열압축

---
+++ 이코테/구현/문자열압축.py
@@ -0,0 +1,35 @@
+def solution(s):
+    result=[]
+    
+    if len(s)==1:
+        return 1
+    
+    for i in range(1, len(s)+1):
+        b = ''
+        cnt = 1
+        tmp=s[:i]
+
+        for j in range(i, len(s)+i, i):
+            
+            if tmp==s[j:i+j]:
+                cnt+=1
+            else:
+                if cnt!=1:
+                    b = b + str(cnt)+tmp
+                else:
+                    b = b + tmp
+                    
+                tmp=s[j:j+i]
+                cnt = 1
+                
+        result.append(len(b))
+    return min(result)
+
+
+s = "aabbaccc"
+# 2a2ba3c
+s = "ababcdcdababcdcd"
+# 2ababcdcd
+s = "abcabcdede"
+# 2abcdede
+print(solution(s))
