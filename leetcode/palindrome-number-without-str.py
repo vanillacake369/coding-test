f058f34a5aff02951696e89d0b6a310fe21b12d5
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-03-20 00:28:53 +0900

    리트코드 > palindrom-number > x 가 reverseX 큰 것을 사용하여 뒤집은숫자 만들기 효율 개선 > 통과

--- leetcode/palindrome-number-without-str.py
+++ leetcode/palindrome-number-without-str.py
@@ -5,27 +5,29 @@ Follow up: Could you solve it without converting the integer to a string?
 
 class Solution(object):
     def isPalindrome(self, x):
-        # initial check : negative cannot be palindrome
-        if (x <0):
+        # initial check : negative, single digit
+        if x < 0 or (x != 0 and x % 10 == 0):
             return False
+
         reversedX = 0
         temp = x
 
         # make reversed number of x, using loop by // 10
-        while(temp != 0):
-            digit = temp % 10
-            reversedX = reversedX * 10 + digit
-            print("digit : " + str(digit))
+        while x > reversedX:
+            reversedX = reversedX * 10 + temp % 10
+            print("temp % 10 : " + str(temp % 10))
             print("reversedX : " + str(reversedX))
             temp = temp // 10
-            print("temp : "+str(temp))
+            print("temp : " + str(temp))
 
-        # check reversedXX 
-        return (reversedX == x)
+        # check reversedX
+        return reversedX == x
+
 
 solution_instance = Solution()
 
-result = solution_instance.isPalindrome(1001)  # Example input value
+result = solution_instance.isPalindrome(121)  # Example input value
+# result = solution_instance.isPalindrome(1001)  # Example input value
 # result = solution_instance.isPalindrome(10)  # Example input value
 # result = solution_instance.isPalindrome(12321)  # Example input value
 
