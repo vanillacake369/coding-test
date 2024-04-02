a7a9835b6f3fae2154bb41756cb9ef15364dcac7
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-04-02 09:40:19 +0900

    백준 > 큐 > 요세푸스

--- programmers/코딩테스트합격자되기_책/큐/요세푸스문제.py
+++ programmers/코딩테스트합격자되기_책/큐/요세푸스문제.py
@@ -1,18 +1,36 @@
-n = int(input())
-k = int(input())
+n, k = map(int, input().split())
+from collections import deque
 
-perm = []
-eliminated = []
 
-now = 0
-for i in range(n):
-    count = 0
-    while count < k:
-        if not now in eliminated:
-            count += 1
-        if count < k:
-            now = (now + 1) % n
-    perm.append(now + 1)
-    eliminated.append(now)
+def solution(n, k):
+    queue = deque()
+    for i in range(1, n + 1):
+        queue.append(i)
+    while len(queue) > 1:
+        for i in range(k - 1):
+            queue.append(queue.popleft())
+            queue.popleft()
+    return queue[0]
 
-print(perm)
+
+print(solution(n, k))
+
+# 아래 풀이는 시간초과가 발생한다.
+# def solution(n, k):
+# perm = []
+# eliminated = []
+#
+# now = 0
+# for i in range(n):
+#     count = 0
+#     while count < k:
+#         if not now in eliminated:
+#             count += 1
+#         if count < k:
+#             now = (now + 1) % n
+#     perm.append(now + 1)
+#     eliminated.append(now)
+#
+# print("<", ", ".join(map(str, perm)), ">", sep='')
+
+solution(n, k)
