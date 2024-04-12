e6aba63fca939fdf0d210ddec2dad2e2b2548209
Laptop-Limjihoon <lonelynight1026@gmail.com>
2024-04-12 12:38:04 +0900

    99클럽스터디 > 미들러 > 대충만든자판.py

---
+++ programmers/99클럽_스터디/미들러/대충만든자판.py
@@ -0,0 +1,23 @@
+def solution(keymap, targets):
+    answer = []
+    keyboard = dict()
+    targetCounts = [0] * len(targets)
+    for key in keymap:
+        for idx, eachKey in enumerate(key):
+            if eachKey in keyboard:
+                keyboard[eachKey] = min(idx + 1, keyboard.get(eachKey))
+            else:
+                keyboard[eachKey] = idx + 1
+    for idx, eachTarget in enumerate(targets):
+        for eachTargetKey in eachTarget:
+            if eachTargetKey in keyboard:
+                targetCounts[idx] += keyboard.get(eachTargetKey)
+            else:
+                targetCounts[idx] = -1
+                break
+    return targetCounts
+
+
+keymap = ["ABACD", "BCEFD"]
+targets = ["ABCD", "AABB"]
+print(solution(keymap, targets))
