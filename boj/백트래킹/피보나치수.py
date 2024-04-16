847d6e663e00ef0eef083b8593d2356bef657113
임지훈 <lonelynight1026@gmail.com>
2024-04-16 14:39:04 +0900

    백준 > 백트래킹 > 피보나치수.py

---
+++ boj/백트래킹/피보나치수.py
@@ -0,0 +1,11 @@
+n = int(input())
+# n = 10
+memo = [0] * (n + 2)
+memo[0] = 0
+memo[1] = 1
+if n == 0 or n == 1:
+    print(memo[n])
+else:
+    for i in range(0, n):
+        memo[i + 2] = memo[i] + memo[i + 1]
+    print(memo[n])
